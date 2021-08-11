b34604caa74237d558f10d15ff0506e53422a889
Teuul <teulery@gmail.com>
2021-08-11 15:10:46 +0200

    first commit: celo election smart-contract event indexer

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,8 +1,40 @@
-import { Exchanged} from '../generated/Exchange/Exchange';
+import { 
+	ValidatorGroupMarkedEligible,
+	ValidatorGroupMarkedIneligible,
+	ValidatorGroupVoteCast,
+	ValidatorGroupVoteActivated,
+	ValidatorGroupPendingVoteRevoked,
+	ValidatorGroupActiveVoteRevoked,
+	EpochRewardsDistributedToVoters
+} from '../generated/Election/Election';
 import { Event, Field, ArrayField, Item } from '../generated/schema';
-import { log, ethereum } from '@graphprotocol/graph-ts';
+import { ethereum } from '@graphprotocol/graph-ts';
 import { handleEvent } from './utils/utils';
 
-export function handleNewExchange(event: Exchanged): void {
-  handleEvent(event,"Exchanged");
+export function handleValidatorGroupMarkedEligible(event: ValidatorGroupMarkedEligible): void {
+  handleEvent(event,"ValidatorGroupMarkedEligible");
+}
+
+export function handleValidatorGroupMarkedIneligible(event: ValidatorGroupMarkedIneligible): void {
+  handleEvent(event,"ValidatorGroupMarkedIneligible");
+}
+
+export function handleValidatorGroupVoteCast(event: ValidatorGroupVoteCast): void {
+  handleEvent(event,"ValidatorGroupVoteCast");
+}
+
+export function handleValidatorGroupVoteActivated(event: ValidatorGroupVoteActivated): void {
+  handleEvent(event,"ValidatorGroupVoteActivated");
+}
+
+export function handleValidatorGroupPendingVoteRevoked(event: ValidatorGroupPendingVoteRevoked): void {
+  handleEvent(event,"ValidatorGroupPendingVoteRevoked");
+}
+
+export function handleValidatorGroupActiveVoteRevoked(event: ValidatorGroupActiveVoteRevoked): void {
+  handleEvent(event,"ValidatorGroupActiveVoteRevoked");
+}
+
+export function handleEpochRewardsDistributedToVoters(event: EpochRewardsDistributedToVoters): void {
+  handleEvent(event,"EpochRewardsDistributedToVoters");
 }
